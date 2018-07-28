668b3ec2df6b374d2b96e791ed5077b25e7c659b
yangkun <594602765@qq.com>
2018-07-28 17:56:45 +0800

    "models create 1"

--- blopApp/App/models.py
+++ blopApp/App/models.py
@@ -2,14 +2,48 @@ import hashlib
 
 from django.db import models
 
+# 用户权限说明
+# 0 阅读
+# 1 发帖
+# 2 删自己的帖子或者修改
+# 3 删别人的帖子，置顶帖子 ，删除不当言论 管理者
+# 4 最高权限 boss
+READ_POWER = 0
+WRITE_POWER = 1
+DEL_SELF_POWER  = 2
+DEL_OTHER_POWER = 3
+TALLER_POWER = 4
+
 # Create your models here.
+class PostModel(models.Model):
+    p_title = models.CharField(max_length=100)  # 标题
+    p_owner = models.CharField(max_length=20)  # 所有者
+    p_comment = models.TextField()              # 内容
+    is_del = models.BooleanField(max_length=False)  # 是否删除
+
+
+    def del_post(self,u_name):
+        user = UserModel.objects.get(u_name=u_name)
+        post_owner = UserModel.objects.get(u_name=self.p_owner)
+
+        if u_name == self.p_owner:
+            return "del ok"
+        elif user.u_authority > post_owner.u_authority:
+            return "del ok"
+        else:
+            return "del failed"
+
+
 class UserModel(models.Model):
     u_name = models.CharField(max_length=20,unique=True)
-    u_icon = models.CharField(max_length=150)
+    u_icon = models.ImageField(upload_to="icons")
     u_password = models.CharField(max_length=256)
     u_email = models.CharField(max_length=32)
+    u_authority = models.IntegerField(default=READ_POWER)   # 用户权限
+    u_posts = models.ForeignKey(PostModel)
     is_del = models.BooleanField(default=False)
     is_act = models.BooleanField(max_length=False)
+
 
     def set_pwd(self,pwd):
         self.u_password = self.generate_pwd(pwd)
@@ -21,3 +55,9 @@ class UserModel(models.Model):
 
     def verify_pwd(self,pwd):
         return self.u_password == self.generate_pwd(pwd)
+
+
+class PostList(models.Model):
+    pl_name = models.CharField(max_length=100)
+    pl_user = models.CharField(max_length=20)
+    pl_user_icon = models.CharField(max_length=100)
--- blopApp/blopApp/settings.py
+++ blopApp/blopApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd%sx%g-tn9!-6j3c$*yy+gw&z_z2wh_2!=htka61p%=&6se8qc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'App.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
